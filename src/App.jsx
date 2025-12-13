ccb9f14ee5fcde70033ff1c7a6fa4f30ff22ef65
awais7796 <awaitzz49@gmail.com>
2025-12-13 18:31:24 +0530

    did routing of home, contact us , about

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" element=<Home /> />
+          <Route path="/services" element=<Services /> />
+          <Route path="/about" element=<About /> />
+          <Route path="/contact" element=<Contact /> />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+export default App;
